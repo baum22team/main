53b3aa978a4b07e5e6f6fada4340710576b96058
MisterSilvereagle <58659498+MisterSilvereagle@users.noreply.github.com>
2020-10-24 10:58:07 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,49 @@
+var timerid;
+var obj;
+var visible = false;
+function toggleFade(objectName) {
+    obj = document.getElementById(objectName);
+    if (obj) {
+        clearTimeout(timerid);
+        if (visible) {
+            hideFade(100);
+            visible = false;
+        }
+        else {  
+            showFade(0);
+            visible = true;
+        }
+    }
+}
 
+function hideFade(opac) {
+    if (obj) {
+        if (opac > 0) {
+            obj.style.opacity = (opac / 100);
+            obj.style.filter = "alpha(opacity=" + opac + ")";
+            timerid = setTimeout("hideFade(" + (opac - 10) + ")",50);
+        }
+        else {
+            obj.style.visibility = "hidden";
+            obj.style.opacity = 1;
+            obj.style.filter = "alpha(opacity=100)";
+            clearTimeout(timerid);
+        }
+    }
+}
+
+function showFade(opac) {
+    if (obj) {
+       if (opac < 100) {
+            obj.style.visibility = "visible";
+            obj.style.opacity = (opac / 100);
+            obj.style.filter = "alpha(opacity=" + opac + ")";
+            timerid = setTimeout("showFade(" + (opac + 10) + ")",50);
+        }
+       else {
+            obj.style.opacity = 1;
+            obj.style.filter = "alpha(opacity=100)";
+            clearTimeout(timerid);
+        }
+    }
+}
